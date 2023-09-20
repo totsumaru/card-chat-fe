34d37684feffbd9f67dfb794adb0af548039e36a
totsuka <techstart35@gmail.com>
2023-09-20 16:29:17 +0900

    修正

--- app/writer/dashboard/[writerId]/page.tsx
+++ app/writer/dashboard/[writerId]/page.tsx
@@ -5,10 +5,9 @@ import React from "react";
 import { Cog6ToothIcon } from "@heroicons/react/24/outline";
 import Container from "@/components/container/Container";
 import Avatar from "@/components/avatar/Avatar";
-import { pathChat } from "@/utils/path";
+import { pathChat, pathDisplayNameEdit } from "@/utils/path";
 import Header from "@/components/header/Header";
-
-const configLink = "/writer/dashboard/w-123/m-123"
+import { SampleData } from "@/utils/sample/Sample";
 
 /**
  * `/writer/dashboard/[writer-id]`
@@ -23,6 +22,8 @@ export default async function Index({
   const supabase = createServerComponentClient({ cookies })
   const { data: { user } } = await supabase.auth.getUser()
 
+  const mock = SampleData
+
   return (
     <div>
       {/* ヘッダー */}
@@ -33,74 +34,53 @@ export default async function Index({
           チャット一覧
         </h1>
 
-        <ul role="list" className="mt-5 divide-gray-100">
-          {comments.map((comment) => (
-            <div className="flex">
-              <Link href={pathChat(comment.id)}>
-                <li key={comment.id} className="flex gap-x-4 p-5 hover:bg-gray-100 border-b border-b-gray-200">
-                  {/* アバター */}
-                  <Avatar/>
+        <ul role="list" className="mt-5">
+          {mock.chat.map((chat) => {
+            const latestMessage = chat.messages[chat.messages.length - 1]
 
-                  <div className="flex-auto">
-                    {/* 上側(表示名+時間) */}
-                    <div className="flex items-baseline justify-between gap-x-4">
-                      <p className="text-sm font-semibold leading-6 text-gray-900">{comment.name}</p>
-                      <p className="flex-none text-xs text-gray-600">
-                        {comment.date}
+            return (
+              <div className="flex hover:bg-gray-100 border-b border-b-gray-200">
+
+                <Link href={pathChat(chat.id)} className="flex-1">
+                  <li key={chat.id} className="flex gap-x-4 p-5 w-full">
+                    {/* アバター */}
+                    <Avatar unreadFlg={!latestMessage.isRead}/>
+
+                    <div className="w-full">
+                      {/* 上側(表示名+時間) */}
+                      <div className="flex items-baseline justify-between gap-x-4">
+                        <p className="text-sm font-semibold leading-6 text-gray-900">
+                          {chat.reader.displayName || chat.id}
+                        </p>
+                        <p className="flex-none text-xs text-gray-600">{latestMessage.date}</p>
+                      </div>
+                      {/* 下側(コメント) */}
+                      <p className={`mt-1 line-clamp-1 text-sm leading-6
+                       ${latestMessage.isRead ? "text-gray-400" : "text-gray-600 font-bold"}`}
+                      >
+                        {latestMessage.content}
                       </p>
                     </div>
-                    {/* 下側(コメント) */}
-                    <p className="mt-1 line-clamp-2 text-sm leading-6 text-gray-600">
-                      {comment.content}
-                    </p>
-                  </div>
 
-                </li>
-              </Link>
-
-              {/* 設定アイコン */}
-              <div className="flex items-center">
-                <Link
-                  href={configLink}
-                  type="button"
-                  className="p-2 text-gray-900 hover:text-blue-600"
-                >
-                  <Cog6ToothIcon className="w-5 h-5"/>
+                  </li>
                 </Link>
-              </div>
 
-            </div>
-          ))}
+                {/* 設定アイコン */}
+                <div className="flex items-center">
+                  <Link
+                    href={pathDisplayNameEdit(writerId, chat.id)}
+                    type="button"
+                    className="p-2 text-gray-900 hover:text-blue-600"
+                  >
+                    <Cog6ToothIcon className="w-5 h-5"/>
+                  </Link>
+                </div>
+
+              </div>
+            )
+          })}
         </ul>
       </Container>
     </div>
   )
 }
-
-const comments = [
-  {
-    id: "2883a5d2-f6be-4908-be04-e1da147f05ef",
-    name: "鈴木様",
-    content: "今日はありがとうございました",
-    date: '1d ago',
-    dateTime: '2023-03-04T15:54Z',
-  }, {
-    id: "d115dca9-2168-445f-8a45-991e748a0665",
-    name: "吉田様",
-    content: "今日はありがとうございました",
-    date: '2d ago',
-    dateTime: '2023-03-03T14:02Z',
-  }, {
-    id: "6a3c8a8e-f158-4ee3-a3dc-a6c626349c7a",
-    name: "斉藤様",
-    content: "今日はありがとうございました",
-    date: '2d ago',
-    dateTime: '2023-03-03T13:23Z',
-  }, {
-    id: "4f250703-023e-4523-81a7-702a9f847e45",
-    name: "田中 和樹",
-    content: "今日はありがとうございました",
-    date: '3d ago',
-    dateTime: '2023-03-02T21:13Z',
-  },
-]
--- components/avatar/Avatar.tsx
+++ components/avatar/Avatar.tsx
@@ -3,26 +3,37 @@ import Link from "next/link";
 type Props = {
   imageUrl?: string
   href?: string
+  unreadFlg?: boolean
 }
 
 // アイコンのアバターです
-export default function Avatar({ imageUrl, href }: Props) {
+export default function Avatar({ imageUrl, href, unreadFlg }: Props) {
+  const noticeIcon = (
+    <span className="absolute right-0 top-0 block h-2.5 w-2.5 rounded-full bg-red-400 ring-2 ring-white"/>
+  )
+
   const avatar = (
     imageUrl ? (
       <div>
-        <img
-          className="inline-block h-10 w-10 rounded-full"
-          src={imageUrl}
-          alt="Avatar"
-        />
+        <span className="relative inline-block">
+          <img
+            className="inline-block h-10 w-10 rounded-full"
+            src={imageUrl}
+            alt="Avatar"
+          />
+          {unreadFlg && noticeIcon}
+        </span>
       </div>
     ) : (
       <div className="">
-        <span className="inline-block h-10 w-10 overflow-hidden rounded-full bg-gray-100">
-          <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-            <path
-              d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"/>
-          </svg>
+        <span className="relative inline-block">
+          <span className="inline-block h-10 w-10 overflow-hidden rounded-full bg-gray-100">
+            <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+              <path
+                d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z"/>
+            </svg>
+          </span>
+          {unreadFlg && noticeIcon}
         </span>
       </div>
     )

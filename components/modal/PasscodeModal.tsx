d3d9b81270ea060395fbd95e90684d39b20ac9ef
totsuka <techstart35@gmail.com>
2023-10-13 15:19:48 +0900

    fix

--- components/modal/PasscodeModal.tsx
+++ components/modal/PasscodeModal.tsx
@@ -84,7 +84,7 @@ export default function PasscodeModal(props: Props) {
             type="text"
             maxLength={passcodeLength}
             pattern="[0-9]*" // 追加: 0-9の数字のみ許可
-            className="block w-full rounded-md border-0 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset
+            className="block w-full rounded-md border px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset
                       ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
                       focus:ring-indigo-600 sm:text-sm sm:leading-6 tracking-widest"
             placeholder="123456"
--- components/modal/StartChatModal.tsx
+++ components/modal/StartChatModal.tsx
@@ -110,7 +110,7 @@ export default function StartChatModal(props: Props) {
 
 // inputのクラス名です
 const inputClassName = `
-block w-full rounded-md border-0 mt-0.5 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset
+block w-full rounded-md border mt-0.5 px-3 py-2 text-gray-900 shadow-sm ring-1 ring-inset
 ring-gray-500 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
 focus:ring-indigo-600 sm:text-sm sm:leading-6
 `
